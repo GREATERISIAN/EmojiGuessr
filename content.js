e57e5e99e5a67811c087365d314803a0d8093200
GREATERISIAN <70813622+GREATERISIAN@users.noreply.github.com>
2021-09-25 20:39:26 -0400

    for the first fork

--- content.js
+++ content.js
@@ -1,4 +1,34 @@
-import emojis from 'emojibase-data/<locale>/data.json';
+
+const url = chrome.runtime.getURL('./data.json');
+
+function loadJSON(filePath) {
+    // Load json file;
+    var json = loadTextFileAjaxSync(filePath, "application/json");
+    // Parse json
+    return JSON.parse(json);
+  }  
+  
+// Load text with Ajax synchronously: takes path to file and optional MIME type
+function loadTextFileAjaxSync(filePath, mimeType)
+{
+  var xmlhttp=new XMLHttpRequest();
+  xmlhttp.open("GET",filePath,false);
+  if (mimeType != null) {
+    if (xmlhttp.overrideMimeType) {
+      xmlhttp.overrideMimeType(mimeType);
+    }
+  }
+  xmlhttp.send();
+  if (xmlhttp.status==200 && xmlhttp.readyState == 4 )
+  {
+    return xmlhttp.responseText;
+  }
+  else {
+    // TODO Throw exception
+    return null;
+  }
+}
+loadJSON('./data.json')
 walk(document.body);
 
 function walk(node) 
